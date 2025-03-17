54805a6592de70c2432b0b273531d8598295976c
Mbowane <2377843@students.wits.ac.za>
2025-03-17 15:45:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(bodyParser.json());
 
-// Root route for testing
 app.get("/", (req, res) => {
     res.send("Book API is running!");
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
